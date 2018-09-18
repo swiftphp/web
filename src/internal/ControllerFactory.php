132a2fa43e2f331146c144fce134c5efd4e91b9a
TomixChiu <qhr2001@yahoo.com>
2018-09-19 00:20:39 +0800

    update ControllerFactory

--- src/internal/ControllerFactory.php
+++ src/internal/ControllerFactory.php
@@ -92,7 +92,7 @@ class ControllerFactory implements IControllerFactory,IConfigurable
 
         //implement IController
         if(!($controller instanceof IController)){
-            throw new \Exception("'".$controllerClass."' controller does not implement interface 'swiftphp\\core\\web\\IController'");
+            throw new \Exception("'".$controllerClass."' controller does not implement interface 'swiftphp\\web\\IController'");
         }
 
         //return
