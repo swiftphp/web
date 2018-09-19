0f6b1715220284337a71f44801b0a8781df7fcea
TomixChiu <qhr2001@yahoo.com>
2018-09-19 19:48:11 +0800

    update HtmlView

--- src/internal/out/HtmlView.php
+++ src/internal/out/HtmlView.php
@@ -349,15 +349,15 @@ class HtmlView extends View implements IOutput
         //占位标签:<page:contentHolder id="" />
         //内容标签:<page:content id="" />
 
-        //模板标签:<page:template file="" />;一个视图最多只存在一个模板
-        $view=$this->loadTemplate($view, $relDir);
-
-        //部件标签:<page:part file="" />
-        $view=$this->loadParts($view, $relDir);
-
         //<taglib prefix="php" namespace="swiftphp\web\tags" />
         //读取标签库后,清空标签库标签
         $view=$this->loadTagLibs($view, $taglibs);
+
+        //模板标签:<page:template file="" />;一个视图最多只存在一个模板
+        $view=$this->loadTemplate($view, $relDir,$taglibs);
+
+        //部件标签:<page:part file="" />
+        $view=$this->loadParts($view, $relDir);
 
         //标签预处理.单标签转为双标签;用占位符统一标记为通用的标签前缀
         $view=$this->preloadTags($view, $taglibs);
@@ -439,9 +439,10 @@ class HtmlView extends View implements IOutput
      * 合并到母板视图(一个视图最多只存在一个模板)
      * @param string $view      视图内容
      * @param string $relDir    视图所在目录
+     * @param array  $taglibs   标签库
      * @throws \Exception
      */
-    protected function loadTemplate($view,$relDir)
+    protected function loadTemplate($view,$relDir,&$taglibs)
     {
         //母板标签:<page:template file="" />;一个视图最多只存在一个母板
         $pattern="/<page:template[^>]{1,}file[\s]*=[\s]*[\"|\']([^\s<>\"\']{1,})[\"|\'][^>]*>/i";
@@ -457,6 +458,9 @@ class HtmlView extends View implements IOutput
 
             //合并母板里的部件
             $template=$this->loadParts($template, dirname($templateFile));
+
+            //读取模板的标签库
+            $template=$this->loadTagLibs($template, $taglibs);
 
             //合并母板到视图
             $view=$this->addTemplateToView($template,$view);
